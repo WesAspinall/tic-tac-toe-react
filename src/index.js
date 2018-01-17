e94508dd79b4bc9a5194fa3e9e4ff8baaf7c305b
wesaspinall <wes@wesaspinall.com>
2018-01-17 02:02:10 -0500

    passed the value prop to the Square component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -10,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
