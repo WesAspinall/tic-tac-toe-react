c3856eebc486955f605930e45ebba1cba17b65bb
wesaspinall <wes@wesaspinall.com>
2018-01-17 03:09:42 -0500

    added calculate winner function

--- src/index.js
+++ src/index.js
@@ -12,6 +12,26 @@ function Square(props) {
     );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 class Board extends React.Component {
 
@@ -43,7 +63,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if(winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -68,6 +95,7 @@ class Board extends React.Component {
   }
 }
 
+
 class Game extends React.Component {
   render() {
     return (
@@ -83,6 +111,8 @@ class Game extends React.Component {
     );
   }
 }
+
+
 
 // ========================================
 
